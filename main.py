56f75faaba33d1815f2e34a97130826a98203ae7
Mehmet Can Gürkök <96959249+mcan22@users.noreply.github.com>
2024-02-26 01:32:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,4 +14,18 @@ while True:
         counter += 1
 
 print("Thanks for playing!")
-print("You got the correct lyrics in", counter, "attempt(s).")
+print("You got the correct first lyrics in", counter, "attempt(s).")
+print()
+print("Time to guess the second lyrics")
+counter2 =1
+while True:
+  lyrics2 = input("Take me to ______ . ")
+  if lyrics2 == "church" or lyrics2 == "Church":
+      print("You got it!")
+      break
+  else:
+      print("Nope! Try again!")
+      counter2 += 1
+print()
+print("Thanks for playing the second round!")
+print("You got the correct second lyrics in", counter2, "attempt(s).")
