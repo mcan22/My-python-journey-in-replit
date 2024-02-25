1165f4d9799cdd45407c98e1f2e7b090486d53cd
Mehmet Can Gürkök <96959249+mcan22@users.noreply.github.com>
2024-02-26 01:24:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
-print("Wholesome Positivity Machine")
-print("who are you?")
-name = input()
-if name== "David" or name== "david":
-  print("What do you want to achieve?")
-  goal = input()
-  print("On a scale of 1 - 10 how do you feel today? (1: 😢, 10: 🥳)")
-  emotion=int(input())
-  if emotion>5:
-    print("perfect")
-  else:
-    print("Can I help you?")
-    getthefeedback=input()
-    if getthefeedback=="yes" or getthefeedback=="Yes":
-      print("Take pills")
+print("Welcome to Name the Song Lyric")
+print()
+print("Figure out the missing word as quickly as you can!")
+print()
+
+counter = 1
+while True:
+    lyrics = input("I don't wanna ______ a thing. ")
+    if lyrics == "miss" or lyrics == "Miss":
+        print("You got it!")
+        break
     else:
-      print("go find someone to help you then mate")
-elif name=="Mark" or name=="mark":
-    print("What do you want to achieve in life my dear brother?")
-    goal = input()
-    print("On a scale of 1 - 10 how do you feel today? (1: 😢, 10: 🥳)")
-    emotion=int(input())
-    if emotion>8:
-      print("You gotta help me then mate i even dont feel that good?")
-    else:
-      print("oh dear brother what happened, can i help")
-      getthefeedback=input()
-      if getthefeedback=="yes" or getthefeedback=="Yes":
-        print("Take a hike")
-      else:
-        print("go find someone to help you then mate")
-else:
-  print("I can only answer questions for Davids or Marks")
+        print("Nope! Try again!")
+        counter += 1
+
+print("Thanks for playing!")
+print("You got the correct lyrics in", counter, "attempt(s).")
