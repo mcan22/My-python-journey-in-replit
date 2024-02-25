e77e9bff5a6353c9da01439a976c9ed85074b50b
Mehmet Can Gürkök <96959249+mcan22@users.noreply.github.com>
2024-02-26 00:24:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
-print("Wholesome Positivity Machine")
-print("who are you?")
-name = input()
-if name== "David" or name== "david":
-  print("What do you want to achieve?")
-  goal = input()
-  print("On a scale of 1 - 10 how do you feel today? (1: 😢, 10: 🥳)")
-  emotion=int(input())
-  if emotion>5:
-    print("perfect")
+
+from getpass import getpass as input
+
+print("E P I C    🪨  📄 ✂️    B A T T L E ")
+print()
+print("Select your move (R, P or S)")
+print()
+
+player1Move = input("Player 1 > ")
+print()
+player2Move = input("Player 2 > ")
+print()
+if player1Move=="R":
+  if player2Move=="R":
+    print("You both picked Rock, draw!")
+  elif player2Move=="S":
+    print("Player1 smashed Player2's Scissors into dust with their Rock!")
+  elif player2Move=="P":
+    print("Player1's Rock is smothered by Player2's Paper!")
   else:
-    print("Can I help you?")
-    getthefeedback=input()
-    if getthefeedback=="yes" or getthefeedback=="Yes":
-      print("Take pills")
-    else:
-      print("go find someone to help you then mate")
-elif name=="Mark" or name=="mark":
-    print("What do you want to achieve in life my dear brother?")
-    goal = input()
-    print("On a scale of 1 - 10 how do you feel today? (1: 😢, 10: 🥳)")
-    emotion=int(input())
-    if emotion>8:
-      print("You gotta help me then mate i even dont feel that good?")
-    else:
-      print("oh dear brother what happened, can i help")
-      getthefeedback=input()
-      if getthefeedback=="yes" or getthefeedback=="Yes":
-        print("Take a hike")
-      else:
-        print("go find someone to help you then mate")
+    print("Invalid Move Player 2!")
+elif player1Move=="P":
+  if player2Move=="R":
+    print("Player2's Rock is smothered by Player1's Paper!")
+  elif player2Move=="S":
+    print("Player1's Paper is cut into tiny pieces by Player2's Scissors!")
+  elif player2Move=="P":
+    print("Two bits of paper flap at each other. Dissapointing. Draw.")
+  else:
+    print("Invalid Move Player 2!")
+elif player1Move=="S":
+  if player2Move=="R":
+    print("Player 2's Rock makes metal-dust out of Player1's Scissors")
+  elif player2Move=="S":
+    print("Ka-Shing! Scissors bounce off each other like a dodgy sword fight! Draw.")
+  elif player2Move=="P":
+    print("Player1's Scissors make confetti out of Player2's paper!")
+  else:
+    print("Invalid Move Player 2!")
 else:
-  print("I can only answer questions for Davids or Marks")
+  print("Invalid Move Player 1!")
+    
+  
