6895b90d357f1778f3fd206f42d2e94d01112815
Mehmet Can Gürkök <96959249+mcan22@users.noreply.github.com>
2024-02-28 22:46:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,15 @@ Increment=int(input( "Increment between values:"))
 
 for i in range(startat,Endbefore,Increment):
   print(i)
+
+print("Welcome to my number list generator.")
+print()
+print("You are going to give me a number you want to start with, an ending number, and by how many you want me to add each time.")
+print()
+
+x = int(input("What number do you want to start with? "))
+y = int(input("What number do you want to end with? "))
+z = int(input("How many should I add each time? "))
+
+for i in range(x, y, z):
+  print(i)
