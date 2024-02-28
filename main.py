661acf1ac44cf97ab307aaeee32cf8f7d3333200
Mehmet Can Gürkök <96959249+mcan22@users.noreply.github.com>
2024-02-28 23:08:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,18 @@
-print("Wholesome Positivity Machine")
-print("who are you?")
-name = input()
-if name== "David" or name== "david":
-  print("What do you want to achieve?")
-  goal = input()
-  print("On a scale of 1 - 10 how do you feel today? (1: 😢, 10: 🥳)")
-  emotion=int(input())
-  if emotion>5:
-    print("perfect")
+print("Math Game!")
+print()
+
+multiples = int(input("Name your multiples: "))
+print()
+
+counter=0
+for i in range(1,11):
+  correct_answer = i*multiples
+  print(i, "x", multiples,"=")
+  answer = int(input("> "))
+  if answer == correct_answer:
+    counter+=1
   else:
-    print("Can I help you?")
-    getthefeedback=input()
-    if getthefeedback=="yes" or getthefeedback=="Yes":
-      print("Take pills")
-    else:
-      print("go find someone to help you then mate")
-elif name=="Mark" or name=="mark":
-    print("What do you want to achieve in life my dear brother?")
-    goal = input()
-    print("On a scale of 1 - 10 how do you feel today? (1: 😢, 10: 🥳)")
-    emotion=int(input())
-    if emotion>8:
-      print("You gotta help me then mate i even dont feel that good?")
-    else:
-      print("oh dear brother what happened, can i help")
-      getthefeedback=input()
-      if getthefeedback=="yes" or getthefeedback=="Yes":
-        print("Take a hike")
-      else:
-        print("go find someone to help you then mate")
-else:
-  print("I can only answer questions for Davids or Marks")
+    print("That's not correct. It should have been", correct_answer)
+if counter > 5:
+  print("You scored", counter, "out of 10. Amazing!")
+  
