7f2dc55e716aeef2e25c27b1fcb8dd5ab40e19df
Mehmet Can Gürkök <96959249+mcan22@users.noreply.github.com>
2024-03-01 23:39:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,14 @@
-print("Wholesome Positivity Machine")
-print("who are you?")
-name = input()
-if name== "David" or name== "david":
-  print("What do you want to achieve?")
-  goal = input()
-  print("On a scale of 1 - 10 how do you feel today? (1: 😢, 10: 🥳)")
-  emotion=int(input())
-  if emotion>5:
-    print("perfect")
+def logSystem():
+  print("Welcome to the log system")
+  print("Please enter your username and password")
+  username = input("Username: ")
+  password = input("Password: ")
+  if username == "admin" and password == "admin":
+    print("Welcome admin")
+  elif username == "user" and password == "user":
+    print("Welcome user")
   else:
-    print("Can I help you?")
-    getthefeedback=input()
-    if getthefeedback=="yes" or getthefeedback=="Yes":
-      print("Take pills")
-    else:
-      print("go find someone to help you then mate")
-elif name=="Mark" or name=="mark":
-    print("What do you want to achieve in life my dear brother?")
-    goal = input()
-    print("On a scale of 1 - 10 how do you feel today? (1: 😢, 10: 🥳)")
-    emotion=int(input())
-    if emotion>8:
-      print("You gotta help me then mate i even dont feel that good?")
-    else:
-      print("oh dear brother what happened, can i help")
-      getthefeedback=input()
-      if getthefeedback=="yes" or getthefeedback=="Yes":
-        print("Take a hike")
-      else:
-        print("go find someone to help you then mate")
-else:
-  print("I can only answer questions for Davids or Marks")
+    print("Invalid username or password")
+
+logSystem()
+  
