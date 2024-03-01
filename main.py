87d1bd5f087624931dc9a24f8ae9ca0e925e21a9
Mehmet Can Gürkök <96959249+mcan22@users.noreply.github.com>
2024-03-01 23:40:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,18 @@ def logSystem():
     print("Invalid username or password")
 
 logSystem()
-  
+
+
+def login():
+  while True:
+    username = input("What is your username?: ")
+    password = input("What is your password? ")
+    if username == "David" and password == "Replit4ev#r":
+      print("Welcome David!")
+      break
+    else:
+      print("That is not the correct username or password. Try again!")
+      continue
+    
+print("REPLIT LOGIN SYSTEM")
+login()
