e258ea0905ae5aa422d1ca0a8e4e6fae022cd65d
Mehmet Can Gürkök <96959249+mcan22@users.noreply.github.com>
2024-03-05 20:44:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,21 @@
-print("Wholesome Positivity Machine")
-print("who are you?")
-name = input()
-if name== "David" or name== "david":
-  print("What do you want to achieve?")
-  goal = input()
-  print("On a scale of 1 - 10 how do you feel today? (1: 😢, 10: 🥳)")
-  emotion=int(input())
-  if emotion>5:
-    print("perfect")
-  else:
-    print("Can I help you?")
-    getthefeedback=input()
-    if getthefeedback=="yes" or getthefeedback=="Yes":
-      print("Take pills")
-    else:
-      print("go find someone to help you then mate")
-elif name=="Mark" or name=="mark":
-    print("What do you want to achieve in life my dear brother?")
-    goal = input()
-    print("On a scale of 1 - 10 how do you feel today? (1: 😢, 10: 🥳)")
-    emotion=int(input())
-    if emotion>8:
-      print("You gotta help me then mate i even dont feel that good?")
-    else:
-      print("oh dear brother what happened, can i help")
-      getthefeedback=input()
-      if getthefeedback=="yes" or getthefeedback=="Yes":
-        print("Take a hike")
-      else:
-        print("go find someone to help you then mate")
-else:
-  print("I can only answer questions for Davids or Marks")
+print("5 Days Down- What did you think?")
+print()
+for i in range(1, 6):
+  print("Day",i,":")
+  userInput=input()
+  print("      You thougt Day",i,"was",userInput)  # Aligned output
+
+
+
+print()
+
+print("10 Days Down - What did you think?")
+print()
+for i in range(1, 11):
+  thought = input(f"Day {i}:\n")
+  print()
+  myText = f"You thought Day {i} was"
+  print(f"{myText:^35}")
+  print(f"{thought:^35}")
+  print()
+
