41664fcdd3a58abdc59aed15a3f4533272d449b6
Mehmet Can Gürkök <96959249+mcan22@users.noreply.github.com>
2024-03-14 01:46:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
-print("Wholesome Positivity Machine")
-print("who are you?")
-name = input()
-if name== "David" or name== "david":
-  print("What do you want to achieve?")
-  goal = input()
-  print("On a scale of 1 - 10 how do you feel today? (1: 😢, 10: 🥳)")
-  emotion=int(input())
-  if emotion>5:
-    print("perfect")
-  else:
-    print("Can I help you?")
-    getthefeedback=input()
-    if getthefeedback=="yes" or getthefeedback=="Yes":
-      print("Take pills")
-    else:
-      print("go find someone to help you then mate")
-elif name=="Mark" or name=="mark":
-    print("What do you want to achieve in life my dear brother?")
-    goal = input()
-    print("On a scale of 1 - 10 how do you feel today? (1: 😢, 10: 🥳)")
-    emotion=int(input())
-    if emotion>8:
-      print("You gotta help me then mate i even dont feel that good?")
-    else:
-      print("oh dear brother what happened, can i help")
-      getthefeedback=input()
-      if getthefeedback=="yes" or getthefeedback=="Yes":
-        print("Take a hike")
-      else:
-        print("go find someone to help you then mate")
-else:
-  print("I can only answer questions for Davids or Marks")
+import os, time
+
+mokedex = {}
+
+def prettyPrint():
+  print(f"Name\tType\tHP\tMP")
+  for key, value in mokedex.items():
+    print(f"""{key:^12}|{value["type"]:^10}|{value["hp"]:^6}|{value["mp"]:^6}""")
+
+while True:
+  print("Add your Beast!")
+  name = input("Name > ").title()
+  type = input("Type > ").title()
+  hp = int(input("HP > "))
+  mp = int(input("MP > "))
+  mokedex[name] = { "type": type, "hp": hp, "mp": mp}
+  print("----------")
+  print()
+  prettyPrint()
