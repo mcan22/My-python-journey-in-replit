7116fcd86303b78bbd7c73aa6e24de2a945a5740
Mehmet Can Gürkök <96959249+mcan22@users.noreply.github.com>
2024-03-14 14:28:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -50,41 +50,46 @@ else:
     print("It's a tie!")
 # More Advanced way of writing it is shown below
 
+import os
+import time
+import random
 
-#import os, time, random
-
-trumps = {}
-trumps["David"] = {"Intelligence": 178, "Speed": 4, "Guile": 80, "Baldness Level": 99}
-trumps["Mr Spock"] = {"Intelligence": 200, "Speed": 50, "Guile": 50, "Baldness Level": 0}
-trumps["Moica from Friends"] = {"Intelligence": 150, "Speed": 50, "Guile": 50, "Baldness Level": 0}
-trumps["Professor X"] = {"Intelligence": 250, "Speed": 1, "Guile": 200, "Baldness Level": 101}
+trumps = {
+    "David": {"Intelligence": 178, "Speed": 4, "Guile": 80, "Baldness Level": 99},
+    "Mr Spock": {"Intelligence": 200, "Speed": 50, "Guile": 50, "Baldness Level": 0},
+    "Monica from Friends": {"Intelligence": 150, "Speed": 50, "Guile": 50, "Baldness Level": 0},
+    "Professor X": {"Intelligence": 250, "Speed": 1, "Guile": 200, "Baldness Level": 101}
+}
 
 while True:
-  print("TOP TRUMPS")
-  print()
-  print("Characters")
-  print()
-  for key in trumps:
-    print(key)
-  user = input("Pick your character\n> ")
-  comp = random.choice(list(trumps.keys()))
-  print("Computer has picked", comp)
+    print("TOP TRUMPS")
+    print()
+    print("Characters")
+    print()
+    for key in trumps:
+        print(key)
+    user = input("Pick your character\n> ")
+    if user in trumps:
+        comp = random.choice(list(trumps.keys()))
+        print("Computer has picked", comp)
 
-  print("Choose your stat: Intelligence, Speed, Guile & Baldness Level")
+        print("Choose your stat: Intelligence, Speed, Guile & Baldness Level")
 
-  answer = input("> ")
+        answer = input("> ")
 
-  print(f"{user}: {trumps[user][answer]}")
-  print(f"{comp}: {trumps[comp][answer]}")
+        print(f"{user}: {trumps[user][answer]}")
+        print(f"{comp}: {trumps[comp][answer]}")
 
-  if trumps[user][answer] > trumps[comp][answer]:
-    print(user, "wins")
-  elif trumps[user][answer] < trumps[comp][answer]:
-    print(comp, "wins")
-  else:
-    print("Draw")
+        if trumps[user][answer] > trumps[comp][answer]:
+            print(user, "wins")
+        elif trumps[user][answer] < trumps[comp][answer]:
+            print(comp, "wins")
+        else:
+            print("Draw")
 
+        time.sleep(2)
+        os.system("clear")
+    else:
+        print("Invalid character. Please pick a character from the list.")
 
-  time.sleep(2)
-  os.system("clear")
 
