45bd5d8b7f052b4755bffef1b1fa03fbf9b1812b
Mehmet Can Gürkök <96959249+mcan22@users.noreply.github.com>
2024-03-19 00:07:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,53 @@
-print("Wholesome Positivity Machine")
-print("who are you?")
-name = input()
-if name== "David" or name== "david":
-  print("What do you want to achieve?")
-  goal = input()
-  print("On a scale of 1 - 10 how do you feel today? (1: 😢, 10: 🥳)")
-  emotion=int(input())
-  if emotion>5:
-    print("perfect")
-  else:
-    print("Can I help you?")
-    getthefeedback=input()
-    if getthefeedback=="yes" or getthefeedback=="Yes":
-      print("Take pills")
-    else:
-      print("go find someone to help you then mate")
-elif name=="Mark" or name=="mark":
-    print("What do you want to achieve in life my dear brother?")
-    goal = input()
-    print("On a scale of 1 - 10 how do you feel today? (1: 😢, 10: 🥳)")
-    emotion=int(input())
-    if emotion>8:
-      print("You gotta help me then mate i even dont feel that good?")
-    else:
-      print("oh dear brother what happened, can i help")
-      getthefeedback=input()
-      if getthefeedback=="yes" or getthefeedback=="Yes":
-        print("Take a hike")
-      else:
-        print("go find someone to help you then mate")
-else:
-  print("I can only answer questions for Davids or Marks")
+import os
+import time
+
+toDoList = []
+file_name = "to_do.txt"
+
+def printList():
+    print()
+    for item in toDoList:
+        print(item)
+    print()
+
+def saveList():
+    with open(file_name, "w") as f:
+        for item in toDoList:
+            f.write(item + "\n")
+
+def loadList():
+    if os.path.exists(file_name):
+        with open(file_name, "r") as f:
+            for line in f:
+                toDoList.append(line.strip())
+
+# Program başlangıcında dosyadan verileri yükle
+loadList()
+
+while True:
+    menu = input("ToDo List Manager\nDo you want to view, add, edit, remove or delete the todo list?\n").lower()
+    if menu == "view":
+        printList()
+    elif menu == "add":
+        item = input("What do you want to add?\n").title()
+        toDoList.append(item)
+        saveList()
+    elif menu == "remove":
+        item = input("What do you want to remove?\n").title()
+        check = input("Are you sure you want to remove this?\n").lower()
+        if check.startswith("y"):
+            if item in toDoList:
+                toDoList.remove(item)
+                saveList()
+    elif menu == "edit":
+        item = input("What do you want to edit?\n").title()
+        new = input("What do you want to change it to?\n").title()
+        for i in range(len(toDoList)):
+            if toDoList[i] == item:
+                toDoList[i] = new
+        saveList()
+    elif menu == "delete":
+        toDoList = []
+        saveList()
+    time.sleep(1)
+    os.system('clear')
