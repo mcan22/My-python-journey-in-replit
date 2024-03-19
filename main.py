600c8c577fbdbc1a665fbde607706ee2b15a1e80
Mehmet Can Gürkök <96959249+mcan22@users.noreply.github.com>
2024-03-19 17:58:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,57 @@
-print("Wholesome Positivity Machine")
-print("who are you?")
-name = input()
-if name== "David" or name== "david":
-  print("What do you want to achieve?")
-  goal = input()
-  print("On a scale of 1 - 10 how do you feel today? (1: 😢, 10: 🥳)")
-  emotion=int(input())
-  if emotion>5:
-    print("perfect")
+import os, time
+pizza = []
+
+
+try:
+  f = open("pizza.txt", "r")
+  pizza = eval(f.read())
+  f.close()
+except:
+  print("ERROR: No existing pizza lsit using a blank list")
+
+def addPizza():
+  time.sleep(1)
+  os.system("clear")
+  name = input("Name: ")
+  size = input("Size (s/m/l): ").lower()
+  while True:
+    try:
+      qty = int(input("Quantity: "))
+      break
+    except:
+      print("Error: Quantity must be a whole number")
+  cost = 0
+  if size == "s":
+    cost = 5.99
+  elif size == "m":
+    cost = 9.99
   else:
-    print("Can I help you?")
-    getthefeedback=input()
-    if getthefeedback=="yes" or getthefeedback=="Yes":
-      print("Take pills")
-    else:
-      print("go find someone to help you then mate")
-elif name=="Mark" or name=="mark":
-    print("What do you want to achieve in life my dear brother?")
-    goal = input()
-    print("On a scale of 1 - 10 how do you feel today? (1: 😢, 10: 🥳)")
-    emotion=int(input())
-    if emotion>8:
-      print("You gotta help me then mate i even dont feel that good?")
-    else:
-      print("oh dear brother what happened, can i help")
-      getthefeedback=input()
-      if getthefeedback=="yes" or getthefeedback=="Yes":
-        print("Take a hike")
-      else:
-        print("go find someone to help you then mate")
-else:
-  print("I can only answer questions for Davids or Marks")
+    cost = 14.99
+  total = cost * qty
+  total = round(total, 2)
+  row = [name, size, qty, total]
+  pizza.append(row)
+
+def viewPizza():
+  h1 = "Name"
+  h2 = "Size"
+  h3 = "Quantity"
+  h4 = "Total"
+  print(f"{h1:^10}{h2:^20}{h3:^10}{h4:^10}")
+  for row in pizza:
+    print(f"{row[0]:^10}{row[1]:^20}{row[2]:^10}{row[3]:^10}")
+  time.sleep(2)
+
+while True:
+  time.sleep(1)
+  os.system("clear")
+  print("Rominos Pizza")
+  print()
+  menu = input("1: Add Pizza\n2: View Pizza\n> ")
+  if menu == "1":
+    addPizza()
+  else:
+    viewPizza()
+  f = open("pizza.txt", "w")
+  f.write(str(pizza))
+  f.close()
